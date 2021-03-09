7b8270c9f824d3d5c78a1d662bb9fe5ad79ac53c
Shubh Pathak <shubhpathak07@gmail.com>
2021-03-09 15:48:15 +0530

    Added Material UI to Login Form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,29 +1,44 @@
 import React, { Component } from "react";
 import axiosInstance from "../axiosApi";
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import UserContext from '../UserContext';
+
+import {TextField, Paper, Grid, Button } from "@material-ui/core"
 
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = { username: "", password: ""};
-        
+        this.state = { username: "", password: "" };
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.isSubmitting = this.isSubmitting.bind(this);
     }
 
     componentDidMount() {
-        if(localStorage.getItem('access_token')){
-            this.setState({isLogin:true});
+        if (localStorage.getItem('access_token')) {
+            this.setState({ isLogin: true });
         }
     }
-  
-    
+
+
     handleChange(event) {
         this.setState({ [event.target.name]: event.target.value });
     }
-    
+
+    handleReset() {
+        this.setState({ username: "", password: "" })
+    }
+
+    isSubmitting() {
+        if (this.state.username == "" || this.state.password == "") {
+            return true;
+        }
+        return false;
+    }
+
     async handleSubmit(event) {
         event.preventDefault();
         try {
@@ -34,30 +49,73 @@ class Login extends Component {
             axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
             localStorage.setItem('access_token', response.data.access);
             localStorage.setItem('refresh_token', response.data.refresh);
-            
+
             this.context.setValue(true);
             return response;
         } catch (error) {
             throw error;
         }
     }
-    
+
+
     render() {
-        if(this.context.value){
-            return <Redirect to='/'/>;
+        if (this.context.value) {
+            return <Redirect to='/' />;
         }
         return (
-            <div>Login
+            <div>
+                <p style={{ textAlign: "center" }}> Login Form </p>
                 <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Nation:
-                        <input name="username" type="text" value={this.state.username} onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Access-Code:
-                        <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
-                    </label>
-                    <input type="submit" value="Submit" />
+                    <Paper style={{ padding: 16 }}>
+                        <Grid container alignItems="center" justify="center" spacing={2}>
+                            <Grid item xs={12} md={8}>
+                                <TextField
+                                    fullWidth
+                                    required
+                                    name="username"
+                                    variant="filled"
+                                    type="text"
+                                    label="Country Name"
+                                    helperText="India,Der Komigin"
+                                    value={this.state.username}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={8}>
+                                <TextField
+                                    fullWidth
+                                    required
+                                    variant="filled"
+                                    id="standard-password-input"
+                                    label="Password"
+                                    type="password"
+                                    name="password"
+                                    autoComplete="current-password"
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+
+                            <Grid item xs={6} style={{ marginTop: 16 }, { marginLeft: 16 }}>
+                                <Button
+                                    type="button"
+                                    variant="contained"
+                                    onClick={this.handleReset}
+                                    disabled={this.isSubmitting()}
+                                >Reset</Button>
+                            </Grid>
+
+                            <Grid item style={{ marginTop: 16 }, { marginLeft: 16 }}>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    type="submit"
+                                    disabled={this.isSubmitting()}
+                                >Submit</Button>
+                            </Grid>
+
+                        </Grid>
+                    </Paper>
                 </form>
             </div>
         )
