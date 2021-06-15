c39e550ec6fbfd7dada2f162155897615c623b26
Shubh Pathak <shubhpathak07@gmail.com>
2021-06-15 17:10:54 +0530

    Implemented SingleQuizMode

--- frontend/src/components/SingleQuiz.js
+++ frontend/src/components/SingleQuiz.js
@@ -18,6 +18,11 @@ const SingleQuiz = () => {
   const [quesList,setQuesList] = useState([]);
   const [quesCount,setQuesCount] = useState(0);
   const [currentQues,setCurrentQues] = useState(initialQuesObj);
+  const [currentExplanation,setCurrentExplanation] = useState("Answer the Question to View Explanation!");
+
+  const [primaryBtnStyle, setPrimaryStyle] = useState("light");
+  const [secondaryBtnStyle, setSecondaryStyle] = useState("light");
+  const [tertiaryBtnStyle, setTertiaryStyle] = useState("light");
 
   const userLogValue = useContext(UserContext);
   
@@ -33,10 +38,57 @@ const SingleQuiz = () => {
       requestData();   
   },[]);
 
-  const handleClick = ()=>{
-      console.log(quesList);
-      console.log(currentQues.explanation);
-      console.log(currentQues);
+  const handleClick = async (event)=>{
+    // Submitting Answer to db and Updating Explanation
+      let answerSelected = event.target.name;
+      const response = await axiosInstance.post('/modes/answer-sq/',{
+        'questionID':currentQues.id,
+        'answer': answerSelected,
+      })
+      if(answerSelected==currentQues.answer){
+        if(answerSelected==1){
+          setPrimaryStyle("success");
+        }
+        else if(answerSelected==2){
+          setSecondaryStyle("success");
+        }
+        else if(answerSelected==3){
+          setTertiaryStyle("success");
+        }
+      }
+      else{
+        if(answerSelected==1){
+          setPrimaryStyle("danger");
+        }
+        else if(answerSelected==2){
+          setSecondaryStyle("danger");
+        }
+        else if(answerSelected==3){
+          setTertiaryStyle("danger");
+        }
+      }
+      document.querySelector("#primarybtn").setAttribute("disabled", "");
+      document.querySelector("#secondarybtn").setAttribute("disabled", "");
+      document.querySelector("#tertiarybtn").setAttribute("disabled", "");
+      setCurrentExplanation(currentQues.explanation);      
+    }
+    
+  const changeNextQuestion = (event)=>{
+    if(quesList.length-1==quesCount){ //last question disable button
+      document.querySelector("#changeNextButton").setAttribute("disabled", "");
+    }
+    document.querySelector("#primarybtn").removeAttribute("disabled", "");
+    document.querySelector("#secondarybtn").removeAttribute("disabled", "");
+    document.querySelector("#tertiarybtn").removeAttribute("disabled", "");
+    
+    setCurrentExplanation("Answer the Question to View Explanation!");
+    setPrimaryStyle("light");
+    setSecondaryStyle("light");
+    setTertiaryStyle("light");
+    let currentCount = quesCount;
+    currentCount+=1;
+    setQuesCount(currentCount);
+    setCurrentQues(quesList[quesCount]);
   }
 
   return (
@@ -63,27 +115,28 @@ const SingleQuiz = () => {
             width: "320px",
             marginTop: "30px",
           }}
-          src=""
-          alt=""
+          src={currentQues.ques_image}
+          alt="No Image For this Question!"
         ></img>
-        <h1 id="img-title">1</h1>
+        <h3 id="img-title">{currentQues.ques_text}</h3>
       </div>
 
       <div style={{ textAlign: "center" }}>
-        <Button onClick={handleClick} className="choicebtn" id="primarybtn" size="lg" variant="light">
+        <Button onClick={handleClick} name='1' className="choicebtn" id="primarybtn" size="lg" variant={primaryBtnStyle}>
           Primary
         </Button>
-        <Button className="choicebtn" size="lg" variant="light">
+        <Button onClick={handleClick} name='2' className="choicebtn" id="secondarybtn" size="lg" variant={secondaryBtnStyle}>
           Secondary
         </Button>
-        <Button className="choicebtn" size="lg" variant="light">
+        <Button onClick={handleClick} name='3' className="choicebtn" id="tertiarybtn" size="lg" variant={tertiaryBtnStyle}>
           Tertiary
         </Button>
       </div>
 
       <div style={{ marginTop: "5%" }}>
         <h1>Explanation</h1>
-        <p>{currentQues.explanation}</p>
+        <p>{currentExplanation}</p>
+        <Button onClick={changeNextQuestion} id="changeNextButton" className="choicebtn" size="sm" variant="light">Next One</Button>
       </div>
     </div>
   )
